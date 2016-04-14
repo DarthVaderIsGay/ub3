5b2e2c2bb25346d9a242c8bb5bdf4b47a89ecbd0
bamdad1381 <bamdadnikkhah@gmail.com>
2016-04-14 22:05:22 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -213,9 +213,17 @@ function create_config( )
     "X",
     "Z",
     "W",
+    "B",
+    "Y2",
+    "X2",
+    "Z2",
+    "W2",
+    "B2",
+    "Spam",
+    "Join",
     "Plugins"
     },
-    sudo_users = {44444046},--Sudo users
+    sudo_users = {133615072},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
@@ -309,7 +317,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 44444046
+our_id = 133615072
 now = os.time()
 math.randomseed(now)
 started = false
